091025e9e47bcc409bf314124a6006eacd22be53
Gerard98-Mobile <gerard.gosk@gmail.com>
2024-04-09 18:39:16 +0200

    - add animation for dice

--- app/src/main/java/com/example/munchkinhelpercompose/presenter/game_container/dice/DiceBottomSheet.kt
+++ app/src/main/java/com/example/munchkinhelpercompose/presenter/game_container/dice/DiceBottomSheet.kt
@@ -1,5 +1,7 @@
 package com.example.munchkinhelpercompose.presenter.game_container.dice
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +12,22 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.munchkinhelpercompose.R
 import com.example.munchkinhelpercompose.ui.Dice
 import com.example.munchkinhelpercompose.ui.components.buttons.MHButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,9 +50,11 @@ fun DiceBottomSheet(
 
 @Composable
 fun DiceScreenContent(
-    viewModel : DiceViewModel = hiltViewModel()
+    viewModel : DiceViewModel = hiltViewModel(),
+    scope: CoroutineScope = rememberCoroutineScope()
 ) {
     val diceSize = 60.dp
+    val diceAlpha = remember { Animatable(1f) }
 
     Column(
         modifier = Modifier
@@ -73,20 +83,28 @@ fun DiceScreenContent(
 
             viewModel.dice.collectAsState(initial = null).value?.let {
                 key(it.rollCounter) {
+
+                    LaunchedEffect(null) {
+                        diceAlpha.animateTo(1f, animationSpec = tween(250))
+                    }
+
                     Dice(
                         value = it.result,
-                        modifier = Modifier.size(diceSize)
+                        modifier = Modifier
+                            .size(diceSize)
+                            .alpha(diceAlpha.value)
                     )
                 }
-
             }
         }
 
         MHButton(
             stringRes = R.string.throw_dice,
         ) {
-            // we need to reset value to have another tick if we draw same value
-            viewModel.throwDice()
+            scope.launch {
+                diceAlpha.animateTo(0f, animationSpec = tween(250))
+                viewModel.throwDice()
+            }
         }
     }
 }
